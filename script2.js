ebfdd196fd1fe8d733f5159a9c12f3e10b49cb7c
Umonomi <ms.kotgermi@gmail.com>
2026-01-14 16:57:56 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,14 @@
+console.log(123);
+
+let num1 = 123;
+console.log(num1);
+
+let num2 = 123;
+let num3 = 456;
+let num4 = 789;
+console.log(num2, num3, num4);
+
+console.log(35325);
+console.log('346234');
+console.log(p);
+console.log(j);
